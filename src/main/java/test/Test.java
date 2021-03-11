7fcec86cd6a30b9ef6616948e5a6f7cfd8b2dbcd
Dini68 <kdini68@gmail.com>
2021-03-11 23:34:36 +0100

    test

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,2 +1,12 @@
-package test;public class Test {
+package test;
+
+public class Test {
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Test().sum(8, 9));
+    }
 }
